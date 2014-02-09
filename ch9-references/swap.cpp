61057d3caae6b28e9769187da99a25e0d37c43b2
Shreyas <seekshreyas@gmail.com>
2014-02-09 04:18:44 -0800

    swap by reference

---
+++ ch9-references/swap.cpp
@@ -0,0 +1,28 @@
+// Passing by reference
+#include <iostream>
+
+using namespace std;
+
+void swap(int *px, int *py);
+
+int main(){
+	int x = 5, y = 10;
+
+	swap(&x, &y);
+
+	cout << "Main - x: " << x << " y: " << y << endl;
+
+	return 0;
+}
+
+
+void swap(int *px, int *py){
+	int temp = 0;
+
+	temp = *px;
+	*px = *py;
+	*py = temp;
+
+	cout << "Swap - *px: " << *px << " *py: " << *py << endl;
+
+}
